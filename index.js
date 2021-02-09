04e2fe4e448beff3e860dc0c4dfa7280ae1f109f
HyeJeongKo-B2LINK <55426349+HyeJeongKo-B2LINK@users.noreply.github.com>
2021-02-09 16:26:47 +0900

    mobile

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 (function(){
     initialSwiper();
-    clickEvent();
+    trendClickEvent();
+    playListClickEvent();
 })();
 
 function initialSwiper(){
@@ -17,7 +18,7 @@ function initialSwiper(){
     },
     });
 
-    var trendSwiper = new Swiper('.trend-section > .swiper-container', {
+    var trendSwiper = new Swiper('.trend-area > .swiper-container', {
         // parallax:true,
         slidesOffsetBefore:20,
         slidesOffsetAfter:20,
@@ -26,16 +27,78 @@ function initialSwiper(){
         spaceBetween:20,
         slidesPerView:2.5,
     });
+
+    var newReleaseSwiper = new Swiper('.new-release > .swiper-container', {
+        slidesOffsetBefore:20,
+        slidesOffsetAfter:20,
+        initialSlide: 0,
+        loop: false,
+        spaceBetween:20,
+        slidesPerView:2.5,
+    });
+
+    var weeklySwiper = new Swiper('.weekly > .swiper-container', {
+        slidesOffsetBefore:20,
+        slidesOffsetAfter:20,
+        initialSlide: 0,
+        loop: false,
+        spaceBetween:20,
+        slidesPerView:2.5,
+    });
+
+    var monthlySwiper = new Swiper('.monthly > .swiper-container', {
+        slidesOffsetBefore:20,
+        slidesOffsetAfter:20,
+        initialSlide: 0,
+        loop: false,
+        spaceBetween:20,
+        slidesPerView:2.5,
+    });
+
+    var playListSwiper = new Swiper('.swiper-container.playlist', {
+        slidesOffsetBefore:20,
+        slidesOffsetAfter:20,
+        initialSlide: 0,
+        loop: false,
+        spaceBetween:20,
+        slidesPerView:1.5,
+    });
 }
 
-function clickEvent(){
+function trendClickEvent(){
     var trendNav = document.querySelector('.trend-nav');
-    console.dir(trendNav,'--trendNav')
-    trendNav.addEventListener('click',function(e){
+    var trends = document.querySelectorAll('.trend-section');
+    trendNav.addEventListener('click',function(e){        
         if(e.target.nodeName !== 'LI') return;
-        for(var i=0;i<= trendNav.legnth;i++){
-            trendNav.children[i].classList.remove('on')
-        }
+
+        var idx = Array.from(trendNav.children).indexOf(e.target)
+        var trendOn = document.querySelector('.trend-nav .on')
+        var activatedTrend = document.querySelector('.trend-section.active');
+        activatedTrend.classList.remove('active');
+        trends[idx].classList.add('active');
+        trendOn.classList.remove('on');
         e.target.classList.add('on')
+    })
+}
+
+function playListClickEvent(){
+    var playlistNav = document.querySelector('.my-nav');
+    var lists = document.querySelectorAll('.lists');
+    playlistNav.addEventListener('click',function(e){
+        var activatedButton = document.querySelector('.my-nav .active');
+        var activatedLists = document.querySelector('.lists.active');
+        // remove previous active
+        activatedButton.classList.remove('active');
+        activatedLists.classList.remove('active');
+        // add new active
+        if(e.target.nodeName === 'BUTTON'){
+            var idx = Array.from(playlistNav.children).indexOf(e.target)
+            e.target.classList.add('active');
+        }else if(e.target.nodeName === 'SPAN'){
+            var idx = Array.from(playlistNav.children).indexOf(e.target.parentNode)    
+            e.target.parentNode.classList.add('active')
+        }
+        lists[idx].classList.add('active');
+
     })
 }
